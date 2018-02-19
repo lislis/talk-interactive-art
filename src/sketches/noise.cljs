09e5f9efe3faecf8a0cba1032fbce47d5b390b46
lislis <mail@lislis.de>
2018-02-19 13:46:23 +0100

    it was a naming thing

--- src/sketches/noise.cljs
+++ src/sketches/noise.cljs
@@ -3,18 +3,16 @@
             [quil.middleware :as m]))
 
 (defn setup []
-  (q/frame-rate 5)
   {})
 
-(defn update-state [state]
-  state)
+(defn update-state [state])
 
 (defn draw-state [state]
   (q/background 240)
-  (q/ellipse 100 100 50 50))
+  )
 
 (q/defsketch sketch
-  :host "noise"
+  :host "canvas-noise"
   :size [700 500]
   :setup setup
   :update update-state
