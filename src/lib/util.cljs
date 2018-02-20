31927c593ae73a26c1fecc1ab39e673e47e11360
lislis <mail@lislis.de>
2018-02-20 14:19:01 +0100

    make it nice

--- src/lib/util.cljs
+++ src/lib/util.cljs
@@ -43,3 +43,60 @@
 
 (defn update-all-noise [dots]
   (map update-noise dots))
+
+(defn create-vector
+  "creates a vector"
+  [x y]
+  {:x x :y y})
+
+(defn add-vector
+  "adds two vectors together"
+  [vec1 vec2]
+  (let [x (+ (:x vec1) (:x vec2))
+        y (+ (:y vec1) (:y vec2))]
+    (create-vector x y)))
+
+(defn wrap-edges "takes location vector" [vec width height]
+  (let [x (cond (> (:x vec) width) 0
+                (< (:x vec) 0) width
+                :else (:x vec))
+        y (cond (> (:y vec) height) 0
+                (< (:y vec) 0) height
+                :else (:y vec))]
+    (create-vector x y)))
+
+(defn apply-force [dot force]
+  (let [acc (add-vector (:acc dot) force)
+        vel (add-vector (:vel dot) acc)
+        loc (add-vector (:loc dot) vel)
+        loc-wrapped (wrap-edges loc (q/width) (q/height))]
+    (assoc dot :loc loc-wrapped)))
+
+(defn apply-all-forces [dots force]
+  (let [updated-dots (map #(apply-force % force) dots)]
+    updated-dots))
+
+(defn alter-wind [wind]
+  (let [x (q/map-range (q/mouse-x) 0 (q/width) -1 1)
+        y (q/map-range (q/mouse-y) 0 (q/height) -1 1)]
+    (if (q/mouse-pressed?)
+      {:x x :y y}
+      wind)))
+
+(defn angle-from-vec [vec]
+  (let [delta-x (:x vec)
+        delta-y (:y vec)
+        rad (q/atan2 delta-y delta-x)]
+    rad))
+
+(defn draw-indicator [vec]
+  (q/fill 230 230 230 0.5)
+  (q/stroke 180)
+  (q/ellipse (/ (q/width) 2) (/ (q/height) 2) 130 130)
+  (q/with-translation [350 250]
+    (q/with-rotation
+      [(angle-from-vec vec)]
+      (q/scale 0.1)
+      (q/fill 180)
+      (q/ellipse (- (q/width) 50) 20 230 230)
+      (q/pop-matrix))))
--- src/sketches/vector.cljs
+++ src/sketches/vector.cljs
@@ -10,41 +10,11 @@
    :wind {:x 0.1
           :y 0.1}})
 
-(defn create
-  "creates a vector"
-  [x y]
-  {:x x :y y})
-
-(defn add
-  "adds two vectors together"
-  [vec1 vec2]
-  (let [x (+ (:x vec1) (:x vec2))
-        y (+ (:y vec1) (:y vec2))]
-    (create x y)))
-
-(defn apply-force [dot force]
-  (let [acc (add (:acc dot) force)
-        vel (add (:vel dot) acc)
-        loc (add (:loc dot) vel)]
-    (assoc dot :loc loc)))
-
-(defn apply-all-forces [dots force]
-  (let [updated-dots (map #(apply-force % force) dots)]
-    updated-dots))
-
-(defn alter-wind [wind]
-  (let [x (q/map-range (q/mouse-x) 0 (q/width) -1 1)
-        y (q/map-range (q/mouse-y) 0 (q/height) -1 1)]
-    (if (q/mouse-pressed?)
-      (do
-        (js/console.log x y)
-        {:x x :y y})
-      wind)))
 
 (defn update-state [state]
   (let [dots (:dots state)
-        wind (alter-wind (:wind state))
-        a (apply-all-forces dots wind)
+        wind (u/alter-wind (:wind state))
+        a (u/apply-all-forces dots wind)
         n (u/update-all-noise a)]
     (assoc state :dots n :wind wind)))
 
@@ -62,7 +32,10 @@
        (q/color-mode :hsb)
        (q/no-stroke)
        (q/fill hue s b)
-       (q/ellipse x y radius radius)))))
+       (q/ellipse x y radius radius))))
+
+  (if (q/mouse-pressed?)
+    (u/draw-indicator (:wind state))))
 
 (q/defsketch sketch
   :host "vector"
